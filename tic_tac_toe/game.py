11384402f5deb77ae741bb39b7e9f0c3bfe64339
Ali Neaz <neazali77@gmail.com>
2021-09-01 21:45:14 +0600

    done the player part

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,8 +1,27 @@
+
+# from tic_tac_toe import HumanPlayer, RandomComputerPlayer
+
+
 class TicTacToe:
     def __init__(self):
-        self.board = [' ' for _ in range(9)]
-        self.curren_winner = None
+        self.board = [" " for _ in range(9)]
+        self.current_winner = None
 
     def print_board(self):
-        for row in [self.board[i*3: (i+1)*3 for i in range(3)]]:
-            print("| "+" |".join(row)+" |")
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' |'.join(row)+' |')
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]
+                        for j in range(3)]
+        for row in number_board:
+            print('| '+' |'.join(row)+' |')
+
+    def availabe_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        # moves = []
+        # for i, spot in enumerate(self.board):
+        #     if spot == ' ':
+        #         moves.append(i)
+        # return moves
